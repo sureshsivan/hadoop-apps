a0a22928ed145e051d2f93aad94e735950e6dbd5
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-02 12:14:17 +0530

    url info updated

---
+++ hadoop-command-ref/others.sh
@@ -0,0 +1,114 @@
+http://s3.amazonaws.com/learn-hadoop/hadoop-infiniteskills-richmorrow-class.tgz;
+http://s3.amazonaws.com/learn-hadoop/4-node-cluster-setup-script.txt
+
+tree /public_dataset/ -d;
+/public_dataset/
+├── infiniteskills
+│   └── code-and-data
+│       ├── code
+│       │   ├── admin-tasks
+│       │   ├── baseball
+│       │   ├── benchmarking
+│       │   ├── etl
+│       │   ├── fair-scheduler
+│       │   ├── flume-basics
+│       │   ├── hdfs-basics
+│       │   ├── hive-basics
+│       │   ├── impala-basics
+│       │   ├── logging
+│       │   ├── partitioner
+│       │   ├── pig-basics
+│       │   ├── sqoop-basics
+│       │   └── wordcount
+│       └── data
+│           ├── baseball
+│           ├── gsod
+│           │   └── 2014
+│           ├── gsod-stations
+│           ├── page_view
+│           ├── shakespeare
+│           ├── shakespeare-hdfs
+│           └── world
+└── weather1
+    └── gsod
+        ├── 1929
+        ├── 1930
+        ├── 1931
+        ├── 1932
+        ├── 1933
+        ├── 1934
+        ├── 1935
+        ├── 1936
+        ├── 1937
+        ├── 1938
+        ├── 1939
+        ├── 1940
+        ├── 1941
+        ├── 1942
+        ├── 1943
+        ├── 1944
+        ├── 1945
+        ├── 1946
+        ├── 1947
+        ├── 1948
+        ├── 1949
+        ├── 1950
+        ├── 1951
+        ├── 1952
+        ├── 1953
+        ├── 1954
+        ├── 1955
+        ├── 1956
+        ├── 1957
+        ├── 1958
+        ├── 1959
+        ├── 1960
+        ├── 1961
+        ├── 1962
+        ├── 1963
+        ├── 1964
+        ├── 1965
+        ├── 1966
+        ├── 1967
+        ├── 1968
+        ├── 1969
+        ├── 1970
+        ├── 1971
+        ├── 1972
+        ├── 1973
+        ├── 1974
+        ├── 1975
+        ├── 1976
+        ├── 1977
+        ├── 1978
+        ├── 1979
+        ├── 1980
+        ├── 1981
+        ├── 1982
+        ├── 1983
+        ├── 1984
+        ├── 1985
+        ├── 1986
+        ├── 1987
+        ├── 1988
+        ├── 1989
+        ├── 1990
+        ├── 1991
+        ├── 1992
+        ├── 1993
+        ├── 1994
+        ├── 1995
+        ├── 1996
+        ├── 1997
+        ├── 1998
+        ├── 1999
+        ├── 2000
+        ├── 2001
+        ├── 2002
+        ├── 2003
+        ├── 2004
+        ├── 2005
+        ├── 2006
+        ├── 2007
+        ├── 2008
+        └── 2009
