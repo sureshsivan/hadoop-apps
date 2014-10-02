3d0652dd86c33e1549c1f71bfb86cc5e62847722
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-02 22:04:31 +0530

    Simple wordcount example start

---
+++ hadoop-wordcount-1/src/main/java/com/suresh/learn/WordCount.java
@@ -0,0 +1,39 @@
+package com.suresh.learn;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
+/**
+ * Created by suren on 2/10/14.
+ */
+public class WordCount {
+
+    public static void main(String [] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        JobConf wordCountJobConf = new JobConf();
+
+        wordCountJobConf.setJarByClass(WordCount.class);
+        wordCountJobConf.setJobName("Simple Word count job");
+        wordCountJobConf.setMapperClass(null);
+        wordCountJobConf.setReducerClass(null);
+//        wordCountJobConf.setPartitionerClass(null);
+//        wordCountJobConf.setCombinerClass(null);
+
+        FileInputFormat.addInputPath(wordCountJobConf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(wordCountJobConf, new Path(args[1]));
+
+        Job wordCountJob = new Job(wordCountJobConf);
+
+        boolean success = wordCountJob.waitForCompletion(true);
+
+        System.exit(success ? 0 : 1);
+
+
+    }
+
+}
