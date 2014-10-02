873b581968c12986e70aef4801fe09061ccfafb5
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-02 22:20:32 +0530

    Using tool runner - as mentioned in Hadoop Definitive Guide..

--- hadoop-wordcount-tool-runner-1/src/main/java/com/suresh/learn/WordCount.java
+++ hadoop-wordcount-tool-runner-1/src/main/java/com/suresh/learn/WordCount.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
 
@@ -17,13 +18,23 @@ public class WordCount extends Configured implements Tool{
 
     @Override
     public int run(String[] args) throws Exception {
-        
+        if (args.length != 2) {
+            System.err.printf("Usage: %s [generic options] <input> <output>\n", getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
 
-        return 0;
+        Job job = new Job(getConf());
+        job.setJarByClass(getClass());
+        org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(args[0]));
+        org.apache.hadoop.mapreduce.lib.output.FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        return job.waitForCompletion(true) ? 0 : 1;
+
     }
 
-    public static void main(String [] args) throws IOException, ClassNotFoundException, InterruptedException {
-
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WordCount(), args);
+        System.exit(exitCode);
     }
 
 }
