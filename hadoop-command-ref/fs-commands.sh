c13a5bf9984b2c51fbabf04b61dd517454772bec
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-02 18:23:58 +0530

    Utility Commands - du added..

--- hadoop-command-ref/fs-commands.sh
+++ hadoop-command-ref/fs-commands.sh
@@ -1,6 +1,9 @@
 sudo -u hdfs hadoop fs -ls hdfs://master1;
 sudo -u hdfs hadoop fs -chown ubuntu:ubuntu hdfs://172.31.18.160/user/ubuntu;
 sudo find / -name "hadoop-examples.jar";
+# Use this to copy all the data from client to HDFS - then use mv command to move it to [valid-data] directory
+hadoop fs -mkdir -p hdfs://master1/user/ubuntu/data/input-data;
+hadoop fs -mkdir -p hdfs://master1/user/ubuntu/data/valid-data;
 hadoop fs -rm -r hdfs://master1/user/ubuntu/data/shakespeare-hdfs1/wordcount-output;
 hadoop jar /opt/cloudera/parcels/CDH-5.1.3-1.cdh5.1.3.p0.12/share/doc/hadoop-0.20-mapreduce/examples/hadoop-examples.jar    \
     wordcount  \
--- hadoop-command-ref/other-util-commands.sh
+++ hadoop-command-ref/other-util-commands.sh
@@ -2,3 +2,5 @@ sudo wget http://s3.amazonaws.com/learn-hadoop/hadoop-infiniteskills-richmorrow-
 tar -xvf hadoop-infiniteskills-richmorrow-class.tgz;
 hadoop fs -help;
 hadoop fs -df;
+hadoop fs -du;
+hadoop fs -df hdfs://master1/user/ubuntu/data;
