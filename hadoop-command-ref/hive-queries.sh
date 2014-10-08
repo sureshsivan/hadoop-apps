cc19ae52bbb0c2c16144ee3b874ab9c88f2ad808
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-08 21:17:44 +0530

    hive queries with staging table. - Weather Countries table

--- hadoop-command-ref/hive-queries.sh
+++ hadoop-command-ref/hive-queries.sh
@@ -1,4 +1,5 @@
 SHOW TABLES;
+SHOW FUNCTIONS;
 
 hive -f ./script.hq
 hive -e 'SELECT * FROM dummy'
@@ -16,7 +17,7 @@ CREATE TABLE
          wban STRING,
          st_name STRING,
          country STRING,
-         fips STRING,
+         fips_id STRING,
          state STRING,
          cal STRING,
          lat STRING,
@@ -46,7 +47,7 @@ LOCATION '/user/ubuntu/data/weather/stations'
              wban,
              st_name,
              country,
-             fips,
+             fips_id,
              state,
              cal,
              lat,
@@ -67,7 +68,57 @@ select st_code, st_name, country, state from weather_stations where st_code !='"
 
 DROP TABLE weather_stations_raw;
 
-hadoop fs -rm -r hdfs://master1/user/ubuntu/data/weather;
+hadoop fs -rm -r hdfs://master1/user/ubuntu/data/weather/staging/stations;
 #######################################################################################
 
 
+
+########################################################################################################################
+##   Country Names
+########################################################################################################################
+SET skip.header.line.count = 1;
+
+CREATE TABLE
+    weather_countries_raw
+        (
+         fips_id STRING,
+         country_name STRING
+        )
+COMMENT 'Country Data Master Table - Raw Data including Header Row'
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'  LINES TERMINATED BY '\n'
+LOCATION '/user/ubuntu/data/weather/staging/countries';
+#TBLPROPERTIES ("skip.header.line.count"="1");
+
+LOAD DATA LOCAL INPATH 'file:///public_dataset/weather1/country-list.txt'
+INTO TABLE weather_countries_raw;
+
+select st_code, st_name, country, state from weather_countries_raw limit 5;
+select st_code, st_name, country, state from weather_countries_raw where fips_id !='"FIPS ID"' limit 5;
+select count(1) from weather_countries_raw where fips_id =='"FIPS ID"';
+select count(1) from weather_countries_raw ;
+
+CREATE TABLE
+    weather_countries
+COMMENT 'Country Data Master Table'
+LOCATION '/user/ubuntu/data/weather/countries'
+    AS
+        SELECT
+             fips_id,
+             country_name
+         FROM
+             weather_countries_raw
+         WHERE
+             fips_id =='"FIPS ID"'
+        ;
+
+select count(1) from weather_countries_raw ;
+select count(1) from weather_countries ;
+
+
+select st_code, st_name, country, state from weather_countries_raw where fips_id =='"FIPS ID"'; limit 5;
+select st_code, st_name, country, state from weather_countries where fips_id =='"FIPS ID"'; limit 5;
+
+DROP TABLE weather_countries_raw;
+
+hadoop fs -rm -r hdfs://master1/user/ubuntu/data/weather/staging/countries;
+#######################################################################################
