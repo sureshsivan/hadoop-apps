0253de3a490f706bba01779aaa105d78b655fb18
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-19 21:15:53 +0530

    HAdoop Programming Skeletons updated

---
+++ hadoop-weather-maxtemp-by-country-station/src/main/java/MaxTempByCountryStation.java
@@ -0,0 +1,24 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+/**
+ * Created by suren on 19/10/14.
+ */
+public class MaxTempByCountryStation extends Configured implements Tool {
+
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int exitCode = ToolRunner.run(new MaxTempByCountryStation(), args);
+        System.exit(exitCode);
+    }
+}
---
+++ hadoop-weather-maxtemp-by-country-station/src/main/java/YearPartitioner.java
@@ -0,0 +1,11 @@
+import org.apache.hadoop.mapreduce.Partitioner;
+
+/**
+ * Created by suren on 19/10/14.
+ */
+public class YearPartitioner extends Partitioner<String, String>{
+    @Override
+    public int getPartition(String s, String s2, int numPartitions) {
+        return 0;
+    }
+}
