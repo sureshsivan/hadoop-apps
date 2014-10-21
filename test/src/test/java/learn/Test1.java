b9fb09bca911d8e564690151eae09513702da97d
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-21 18:30:39 +0530

    RawComparator and WritableComparator Test

--- test/src/test/java/learn/Test1.java
+++ test/src/test/java/learn/Test1.java
@@ -1,9 +1,6 @@
 package learn;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.util.StringUtils;
 import org.junit.Test;
 
@@ -18,44 +15,49 @@ public class Test1 {
     @Test
     public void testIntegerToHex() throws IOException {
 //        System.out.println(StringUtils.("1"));
-        IntWritable i1 = new IntWritable(-2);
+        IntWritable i1 = new IntWritable(-5);
         Text t = new Text();
         t.set("211");
         IntWritable i2 = new IntWritable(2);
         LongWritable l1 = new LongWritable(1L);
         LongWritable l2 = new LongWritable(2L);
 
-        System.out.println(serialize(i1));
-        System.out.println(serialize(i1).length);
-        System.out.println(serialize(i2));
-        System.out.println(serialize(i2).length);
-        System.out.println(serialize(l1));
-        System.out.println(serialize(l1).length);
-        System.out.println(serialize(l2));
-        System.out.println(serialize(l2).length);
-        System.out.println(serialize(t));
-        System.out.println(serialize(t).length);
+        RawComparator<IntWritable> comp = WritableComparator.get(IntWritable.class);
+        System.out.println(comp.compare(i1, i2));
+        System.out.println(comp.compare(serialize(i1), 0, 0, serialize(i2), 0, 0));
 
 
-        System.out.println("#E#############################");
-        for (byte b : serialize(t)){
-            System.out.println(b);
-        }
-        System.out.println("#E#############################");
-
-        byte b = 127;
-
-        System.out.println("#E#############################");
-        System.out.println(StringUtils.byteToHexString(serialize(i1)));
-        System.out.println(StringUtils.byteToHexString(serialize(t)));
-        System.out.println(StringUtils.byteToHexString(serialize(i2)));
-        System.out.println(StringUtils.byteToHexString(serialize(l1)));
-        System.out.println(StringUtils.byteToHexString(serialize(l2)));
-
-        System.out.println("#E#############################");
-
-
-        System.out.println(StringUtils.byteToHexString(org.apache.commons.codec.binary.StringUtils.getBytesUtf8("1")));
+//        System.out.println(serialize(i1));
+//        System.out.println(serialize(i1).length);
+//        System.out.println(serialize(i2));
+//        System.out.println(serialize(i2).length);
+//        System.out.println(serialize(l1));
+//        System.out.println(serialize(l1).length);
+//        System.out.println(serialize(l2));
+//        System.out.println(serialize(l2).length);
+//        System.out.println(serialize(t));
+//        System.out.println(serialize(t).length);
+//
+//
+//        System.out.println("#E#############################");
+//        for (byte b : serialize(t)){
+//            System.out.println(b);
+//        }
+//        System.out.println("#E#############################");
+//
+//        byte b = 127;
+//
+//        System.out.println("#E#############################");
+//        System.out.println(StringUtils.byteToHexString(serialize(i1)));
+//        System.out.println(StringUtils.byteToHexString(serialize(t)));
+//        System.out.println(StringUtils.byteToHexString(serialize(i2)));
+//        System.out.println(StringUtils.byteToHexString(serialize(l1)));
+//        System.out.println(StringUtils.byteToHexString(serialize(l2)));
+//
+//        System.out.println("#E#############################");
+//
+//
+//        System.out.println(StringUtils.byteToHexString(org.apache.commons.codec.binary.StringUtils.getBytesUtf8("1")));
     }
 
     public static byte[] serialize(Writable writable) throws IOException {
