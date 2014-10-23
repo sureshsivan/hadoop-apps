e680e985b53a76dd0347270a4d0d1b900cbcc754
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-23 18:44:55 +0530

    Home Machine Changes

--- test/src/test/java/learn/Test1.java
+++ test/src/test/java/learn/Test1.java
@@ -1,6 +1,10 @@
 package learn;
 
+import org.apache.avro.Schema;
 import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.serializer.Deserializer;
+import org.apache.hadoop.io.serializer.Serialization;
+import org.apache.hadoop.io.serializer.Serializer;
 import org.apache.hadoop.util.StringUtils;
 import org.junit.Test;
 
@@ -15,17 +19,48 @@ public class Test1 {
     @Test
     public void testIntegerToHex() throws IOException {
 //        System.out.println(StringUtils.("1"));
-        IntWritable i1 = new IntWritable(-5);
+//        IntWritable i1 = new IntWritable(-5);
+//        Text t = new Text();
+//        t.set("211");
+//        IntWritable i2 = new IntWritable(2);
+//        LongWritable l1 = new LongWritable(1L);
+//        LongWritable l2 = new LongWritable(2L);
+//        VIntWritable
+//        RawComparator<IntWritable> comp = WritableComparator.get(IntWritable.class);
+//        System.out.println(comp.compare(i1, i2));
+//        System.out.println(comp.compare(serialize(i1), 0, 0, serialize(i2), 0, 0));
+
         Text t = new Text();
-        t.set("211");
-        IntWritable i2 = new IntWritable(2);
-        LongWritable l1 = new LongWritable(1L);
-        LongWritable l2 = new LongWritable(2L);
+        t.set("SURESH" +
+                "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+                "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" +
+//                "xdbndfxgndfgndxs;wlfgknhbaszljnfbljkaszdfnbajc nvljasfdhgashdofr ikgshdfghbsajfcg hnmakoi moiasugh oiusahngoiuwerhcgoiuwcjmhoiuwtehytnowiuryntfcoquwirvytowiuerytowiuerythio" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+//                "aswruhgtfqaouiwerhc oqurtyhfoiuweryhtoiuweyvtoiwueytoiqwuerhgtowiughtvoiwerytvowieurytvoqiwueryt5voqiuwerytoqiuwehrfpoqIWERBHGFOIQWHVCGFNPOIQWERUHTPQO WUEHYTPOQWUEHTPOQWVNPIOWUERHTEWA" +
+                "");
+        byte[] ba = serialize(t);
+//        for (byte b : ba){
+//            System.out.println(b);
+//        }
+        System.out.println(t.getLength());
+//        System.out.println(t.);
+        System.out.println(ba.length);
+        WritableComparator wc = WritableComparator.get(Text.class);
+        System.out.println(WritableUtils.decodeVIntSize(ba[0]));
+        System.out.println(WritableComparator.readVInt(ba, 0));
 
-        RawComparator<IntWritable> comp = WritableComparator.get(IntWritable.class);
-        System.out.println(comp.compare(i1, i2));
-        System.out.println(comp.compare(serialize(i1), 0, 0, serialize(i2), 0, 0));
-
+//        Serialization
+//        Serializer
+//        Deserializer
+//        Schema.Parser
 
 //        System.out.println(serialize(i1));
 //        System.out.println(serialize(i1).length);
