e16c78c4e750ac36b7132133e8c1b7d01f515107
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-10-28 17:05:04 +0530

    mvn commands update

--- hadoop-command-ref/[104]weather-hadoop-jar.sh
+++ hadoop-command-ref/[104]weather-hadoop-jar.sh
@@ -1,7 +1,11 @@
 clone from github;
 cd /tmp;
-rm -r ./hadoop-apps;
+rm -r /tmp/hadoop-apps;
 git clone https://github.com/v8-suresh/hadoop-apps.git;
+
+rm -r /tmp/hadoop-apps;
+
+
 cd hadoop-apps;
 mvn package;
 hadoop jar ./hadoop-weather-maxtemp-by-country-station/target/hadoopMaxTempByCountryStation-1.0-SNAPSHOT.jar WeatherDataParserDriver hdfs://localhost/data/weather/prod/weather_data.tsv ;
