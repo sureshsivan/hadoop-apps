b7d1ca9661feeb080a0d38a35bb4403a30732335
v8-suresh <suresh.sivanantham@gmail.com>
2015-02-15 21:11:46 +0530

    Few Lines of code on hadoop Configuration API

---
+++ hadoop-fs-access/src/test/java/learn/HadoopConfigurationTest.java
@@ -0,0 +1,32 @@
+package learn;
+
+import org.apache.hadoop.conf.Configuration;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by suren on 15/2/15.
+ */
+public class HadoopConfigurationTest {
+
+    @Test
+    public void testHAdoopConfigurationApi(){
+        Configuration conf = new Configuration();
+        conf.addResource("test-config.xml");
+
+        Iterator<Map.Entry<String, String>> it = conf.iterator();
+
+        while(it.hasNext()){
+            Map.Entry<String, String> prop = it.next();
+            System.out.println("####################################");
+            System.out.println(prop.getKey());
+            System.out.println(prop.getValue());
+            System.out.println("####################################");
+        }
+
+        System.out.println(conf);
+
+    }
+}
---
+++ hadoop-wordcount-1/src/main/java/com/suresh/learn/TestSample.java
@@ -0,0 +1,27 @@
+package com.suresh.learn;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.hdfs.DFSInputStream;
+import org.apache.hadoop.hdfs.DFSOutputStream;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+
+/**
+ * Created by suren on 15/2/15.
+ */
+public class TestSample {
+
+
+    public void sampleTest(){
+//        SequenceFile
+//        FSDataInputStream
+//        DistributedFileSystem
+//        DFSInputStream
+//        DFSOutputStream
+
+
+    }
+
+}
